9f263bed17c963e3992d339fec5b00165529d49a
Edwin Ortega Caicedo <edwinorte@gmail.com>
2021-05-24 13:11:18 -0500

    tenia un error en el wondows pto locatin

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-var url = window.location.url;
+var url = window.location.href;
 var swLocation = '/Twittor/sw.js';
 
 if( navigator.serviceWorker ){
-    
-if( url.includes( 'localhost')){
-    swLocation = '/sw.js';
-}
+
+    if( url.includes( 'localhost')){
+        swLocation = '/sw.js';
+    }
 
     navigator.serviceWorker.register( swLocation );
 }
